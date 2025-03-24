54f7d21855866973ff4d9499f05a629a55dfaf64
hchowdhury <hc1415926@gmail.com>
2025-03-24 15:07:46 +0000

    Added further comments to define class methods

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,26 +1,20 @@
-
 /**
- * Horse defines the attributes and actions of a 
- * race horse. 
- * 
- * Horse attributes should only be accessed using 
- * the methods provided.
+ * Defines the attributes and actions of a race horse.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.1
+ * @version Version 1.2
  */
 public class Horse
 {
     //Fields of class Horse
     private char horseSymbol;
-    private String horseName;
+    private final String horseName;
     private double horseConfidence;
     private int distanceTravelled;
     private boolean fallen;
 
 
-
-    //Constructor of class Horse
+    // Constructor of class Horse
     /**
      * Constructor for objects of class Horse
      */
@@ -34,51 +28,88 @@ public class Horse
     }
     
     
-    
-    //Other methods of class Horse
+    /**
+     * Marks the horse as fallen.
+     */
     public void fall()
     {
         this.fallen = true;
-        return;
     }
     
+
+    /**
+     * Gets the horse's confidence level.
+     * @return A double representing the horse's confidence level.
+     */
     public double getConfidence()
     {
         return this.horseConfidence;
     }
-    
+
+
+    /**
+     * Gets the distance travelled by the horse.
+     * @return An integer representing the distance travelled in metres.
+     */
     public int getDistanceTravelled()
     {
         return this.distanceTravelled;
     }
+
     
+    /**
+     * Gets the name of the horse.
+     * @return A String representing the horse's name.
+     */
     public String getName()
     {
         return this.horseName;
     }
     
+
+    /**
+     * Gets the symbol representing the horse.
+     * @return A char symbol representing the horse.
+     */
     public char getSymbol()
     {
         return this.horseSymbol;
     }
     
+
+    /**
+     * Resets the horse's distance travelled to 0.
+     */
     public void goBackToStart()
     {
         this.distanceTravelled = 0;
-        return;
     }
     
+
+    /**
+     * Gets the status of the horse.
+     * @return A boolean representing true if the horse has fallen
+     * and false if otherwise.
+     */
     public boolean hasFallen()
     {
         return this.fallen;
     }
 
+
+    /**
+     * Increments the horse's distance travelled by 1 metre.
+     */
     public void moveForward()
     {
         this.distanceTravelled++;
-        return;
     }
 
+
+    /**
+     * Sets the horse's confidence level to a new value between 0 and 1.
+     * @param newConfidence The new confidence level.
+     */
     public void setConfidence(double newConfidence)
     {
         if (newConfidence >= 0 && newConfidence <= 1) {
@@ -86,10 +117,15 @@ public class Horse
         }
     }
     
+
+    /**
+     * Sets the symbol representing the horse to a new character.
+     * @param newSymbol The new representing symbol.
+     */
     public void setSymbol(char newSymbol)
     {
         this.horseSymbol = newSymbol;
-        return;
     }
     
+
 } // END class Horse
