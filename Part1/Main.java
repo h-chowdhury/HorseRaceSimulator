dfd64a5805a1527840386b2f689e69734cddaaf5
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-13 19:29:17 +0600

    Changed input methods to use input helper class

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,11 +1,9 @@
-import java.util.Scanner;
-
 /**
  * This is the main class of the program.
  * It currently acts as the main entry point.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.2
+ * @version Version 1.3
  */
 public class Main
 {
@@ -16,20 +14,14 @@ public class Main
   public static void main(String[] args) 
   {
     // User input for all fields:
-    Scanner scanner = new Scanner(System.in);
+    InputHelper inputHelper = new InputHelper();
 
     System.out.println("Welcome to the Horse Race Simulator!\n");
-
-    System.out.println("\nEnter the distance of the race (m): ");
-    int distance = Integer.parseInt(scanner.nextLine());
-
-    System.out.println("\nEnter the number of horses: ");
-    int noOfHorses = Integer.parseInt(scanner.nextLine());
-
+    int distance = inputHelper.getPosInt("\nEnter the distance of the race (m): ");
+    int noOfHorses = inputHelper.getPosInt("\nEnter the number of horses: ");
 
     // Create race object
     Race race = new Race(distance, noOfHorses);
-
 
     // Create Horse objects
     Horse horse;
@@ -37,26 +29,21 @@ public class Main
 
     for (int i=0; i<noOfHorses; i++) {
       System.out.println("\n\nHORSE #" + ((int) i+1));
-      System.out.println("Enter the symbol representing the horse: ");
-      char symbol = scanner.nextLine().charAt(0);
-
-      System.out.println("Enter the horse's name: ");
-      String name = scanner.nextLine();
-
-      System.out.println("Enter the horse's confidence (0-1): ");
-      double confidence = Double.parseDouble(scanner.nextLine());
+      char symbol = inputHelper.getChar("Enter the symbol representing the horse: ");
+      String name = inputHelper.getString("Enter the horse's name: ");
+      double confidence = inputHelper.getConfidence("Enter the horse's confidence (0-1): ");
 
       horse = new Horse(symbol, name, confidence);
       horses[i] = horse;
     }
 
-
     // Add horses to the race
     for (int i=0; i<noOfHorses; i++) {
-      race.addHorse(horses[i], i);
+      race.addHorse(horses[i], i+1);
     }
 
     // Start the race
     race.startRace();
   }
+
 }
