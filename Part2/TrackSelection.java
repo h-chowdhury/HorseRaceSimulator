77b92334b15a54da24a3b2fe7d5c897fcffc53cc
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 00:06:09 +0600

    Added a slider for selecting number of horses

--- Part2/TrackSelection.java
+++ Part2/TrackSelection.java
@@ -9,16 +9,47 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
- * This class defines the track selection window of the program
+ * This class defines the track selection window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.0
+ * @version Version 1.1
  */
-public class TrackSelection extends JFrame {
+public class TrackSelection extends JFrame implements ChangeListener {
+
+  JLabel noOfTracksLabel;
+  JSlider noOfTracksSlider;
 
   public TrackSelection() {
 
+    noOfTracksLabel = new JLabel();
+
+    noOfTracksSlider = new JSlider(1, 15, 1);
+    noOfTracksSlider.setPreferredSize(new Dimension(400, 200));
+    noOfTracksSlider.setPaintTicks(true);
+    noOfTracksSlider.setMinorTickSpacing(1);
+    noOfTracksSlider.setPaintTrack(true);
+    noOfTracksSlider.setMajorTickSpacing(1);
+    noOfTracksSlider.setPaintLabels(true);
+    noOfTracksSlider.addChangeListener(this);
+
+    noOfTracksLabel.setText(noOfTracksSlider.getValue() + " horses");
+  
+    this.setTitle("Horse Racing Simulator"); 
+    this.setVisible(true); 
+    this.setSize(1000, 800);
+    this.setResizable(false); // prevents resizing window
+    this.setLayout(new java.awt.GridLayout(2, 1, 5, 5));
+    this.getContentPane().setBackground(Color.white);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    this.add(noOfTracksLabel);
+    this.add(noOfTracksSlider);
+  }
+
+  @Override 
+  public void stateChanged(ChangeEvent e) {
+    noOfTracksLabel.setText(noOfTracksSlider.getValue() + " horses");
+  }
 
   
-}
 }
