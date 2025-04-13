88de4456c2f1202d48f43b1f85fc64bd0b529aa3
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-13 23:54:51 +0600

    Created TrackSelection class to define a new window

---
+++ Part2/TrackSelection.java
@@ -0,0 +1,24 @@
+package Part2;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * This class defines the track selection window of the program
+ * 
+ * @author Humayra Chowdhury
+ * @version Version 1.0
+ */
+public class TrackSelection extends JFrame {
+
+  public TrackSelection() {
+
+
+  
+}
+}
