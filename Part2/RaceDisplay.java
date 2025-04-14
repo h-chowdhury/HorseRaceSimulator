22ec3540c71075cea2785ee7b29451263eb54887
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 17:02:46 +0600

    Integrated usage of RaceData class into RaceDisplay, adding a private variable to store previously collected data

--- Part2/RaceDisplay.java
+++ Part2/RaceDisplay.java
@@ -8,18 +8,21 @@ import javax.swing.JFrame;
  * This class defines the race display window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.0
+ * @version Version 1.1
  */
 public class RaceDisplay extends JFrame {
 
-  public RaceDisplay () {
-          // Creating main frame
-          this.setTitle("Horse Racing Simulator"); 
-          this.setVisible(true); 
-          this.setSize(1000, 800);
-          this.setResizable(false); // prevents resizing window
-          this.getContentPane().setBackground(Color.white);
-          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          this.setLayout(new BorderLayout(5, 5));
+  public RaceDisplay (RaceData RD) {
+
+    RaceData raceData = RD;
+
+    // Creating main frame
+    this.setTitle("Horse Racing Simulator - Race Display"); 
+    this.setVisible(true); 
+    this.setSize(1000, 800);
+    this.setResizable(false); // prevents resizing window
+    this.getContentPane().setBackground(Color.white);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(new BorderLayout(5, 5));
   }
 }
