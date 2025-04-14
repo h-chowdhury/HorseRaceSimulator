6d55bc11f4e4c9b299918e77c986e2ee6c055cec
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 16:22:26 +0600

    Integrated RaceData class to HorseSelection class constructor

--- Part2/HorseSelection.java
+++ Part2/HorseSelection.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
  * This class defines the horse selection window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.1
+ * @version Version 1.2
  */
 public class HorseSelection extends JFrame {
 
@@ -16,12 +16,12 @@ public class HorseSelection extends JFrame {
   private int lengthOfTrack;
   private int numberOfLanes;
 
-  public HorseSelection(int numberOfHorses, int lengthOfTrack, int numberOfLanes) {
+  public HorseSelection(RaceData raceData) {
 
     // Storing the selection values from previous page
-    this.numberOfHorses = numberOfHorses;
-    this.lengthOfTrack = lengthOfTrack;
-    this.numberOfLanes = numberOfLanes;
+    this.numberOfHorses = raceData.getNumberOfHorses();
+    this.lengthOfTrack = raceData.getLengthOfTrack();
+    this.numberOfLanes = raceData.getNumberOfLanes();
 
     // Creating main frame
     this.setTitle("Horse Racing Simulator"); 
