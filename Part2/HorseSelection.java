7d3c01e5c638232509d43b407b92e022f30aa0f9
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 16:08:07 +0600

    Added private instance variables to store input data from TrackSelection window

--- Part2/HorseSelection.java
+++ Part2/HorseSelection.java
@@ -8,11 +8,20 @@ import javax.swing.JFrame;
  * This class defines the horse selection window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.0
+ * @version Version 1.1
  */
 public class HorseSelection extends JFrame {
 
-  public HorseSelection() {
+  private int numberOfHorses;
+  private int lengthOfTrack;
+  private int numberOfLanes;
+
+  public HorseSelection(int numberOfHorses, int lengthOfTrack, int numberOfLanes) {
+
+    // Storing the selection values from previous page
+    this.numberOfHorses = numberOfHorses;
+    this.lengthOfTrack = lengthOfTrack;
+    this.numberOfLanes = numberOfLanes;
 
     // Creating main frame
     this.setTitle("Horse Racing Simulator"); 
