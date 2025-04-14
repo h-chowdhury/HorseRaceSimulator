760c3310f4b9173d66c36a664b1c730a791ea762
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 20:04:32 +0600

    Added 5 basic panels to the HorseSelection window

--- Part2/HorseSelection.java
+++ Part2/HorseSelection.java
@@ -2,17 +2,19 @@ package Part2;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * This class defines the horse selection window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.5
+ * @version Version 1.6
  */
 public class HorseSelection extends JFrame {
 
@@ -26,11 +28,29 @@ public class HorseSelection extends JFrame {
     // Creating main frame
     this.setTitle("Horse Racing Simulator - Horse Selection"); 
     this.setVisible(true); 
-    this.setSize(1000, 800);
+    this.setSize(1500, 800);
     this.setResizable(false); // prevents resizing window
     this.getContentPane().setBackground(Color.white);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLayout(new BorderLayout(5, 5));
+    this.setLayout(new BorderLayout(10, 10));
+
+    JPanel titlePanel = new JPanel();
+    JPanel leftColumnPanel = new JPanel(); 
+    JPanel rightColumnPanel = new JPanel();
+    JPanel bottomPanel = new JPanel();
+    JPanel centerPanel = new JPanel();
+
+    titlePanel.setPreferredSize(new Dimension(100, 100));
+    leftColumnPanel.setPreferredSize(new Dimension(400, 100));
+    rightColumnPanel.setPreferredSize(new Dimension(400, 100));
+    bottomPanel.setPreferredSize(new Dimension(100, 200));
+    centerPanel.setPreferredSize(new Dimension(100, 100));
+
+    this.add(titlePanel, BorderLayout.NORTH);
+    this.add(leftColumnPanel, BorderLayout.WEST);
+    this.add(rightColumnPanel, BorderLayout.EAST);
+    this.add(bottomPanel, BorderLayout.SOUTH);
+    this.add(centerPanel, BorderLayout.CENTER);
 
     // Submit button - submit all data
     JButton submitButton = new JButton("Submit");
@@ -50,7 +70,7 @@ public class HorseSelection extends JFrame {
       }
     });
 
-    this.add(submitButton);
+    bottomPanel.add(submitButton);
 
   }
 
