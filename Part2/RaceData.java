5385a47fdb0bbacf3e07104f97eed7399d763981
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-14 16:15:20 +0600

    Created a RaceData class to store data about a race concisely

---
+++ Part2/RaceData.java
@@ -0,0 +1,18 @@
+package Part2;
+
+/**
+ * This class defines the variables involved in a race.
+ * It stores the data which can be accessed with getters
+ * and setters.
+ * 
+ * @author Humayra Chowdhury
+ * @version Version 1.0
+ */
+public class RaceData {
+  private int numberOfHorses;
+  private int lengthOfTrack;
+  private int numberOfLanes;
+  private String trackShape;
+  private String weather;
+  private String raceName;
+}
