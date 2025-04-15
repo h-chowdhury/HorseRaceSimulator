ad5c9d980268313cb17a122b05f674dadfb815ab
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-15 18:35:17 +0600

    Added page title to RaceDisplay window

--- Part2/RaceDisplay.java
+++ Part2/RaceDisplay.java
@@ -2,13 +2,18 @@ package Part2;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * This class defines the race display window of the program.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.1
+ * @version Version 1.2
  */
 public class RaceDisplay extends JFrame {
 
@@ -24,5 +29,17 @@ public class RaceDisplay extends JFrame {
     this.getContentPane().setBackground(Color.white);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new BorderLayout(5, 5));
+
+    // Title panel
+    JPanel titlePanel = new JPanel();
+    titlePanel.setPreferredSize(new Dimension(100, 100));
+
+    JLabel title = new JLabel("Horse Race");
+    title.setFont(new Font("Dialog", Font.BOLD, 50));
+
+    this.add(titlePanel, BorderLayout.NORTH);
+    titlePanel.add(title);
+
+
   }
 }
