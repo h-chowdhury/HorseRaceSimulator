d6a15df77d686f2b7b3fffac56012526d1f2868e
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-16 13:58:25 +0600

    Changed positional and image values to arrays allowing for several horses to be animated simultaneously

--- Part2/RacePanel.java
+++ Part2/RacePanel.java
@@ -16,49 +16,64 @@ import javax.swing.JPanel;
  * The horse race animation will be displayed within this panel.
  * 
  * @author Humayra Chowdhury
- * @version Version 1.3
+ * @version Version 1.4
  */
 public class RacePanel extends JPanel implements ActionListener {
-
-  private RaceData raceData;
-
   final int PANEL_HEIGHT = 620;
   final int PANEL_WIDTH = 1090;
 
-  Image horse;
+  private RaceData raceData;
+  private int numberOfHorses;
+
+  Image[] horses;
   Timer timer;
-  int xVelocity = 5;
-  int yVelocity = 1;
-  int x = 0;
-  int y = 0;
+  int[] xVelocity = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+  int[] yVelocity = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  int[] xPos = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+  int[] yPos = {10, 45, 80, 115, 150, 185, 215, 255, 290, 325, 360, 395, 430, 465, 500};
+
+  final int FINISH_LINE_X = PANEL_WIDTH -60; 
 
   RacePanel (RaceData RD) {
 
-    this.raceData = RD;
+    raceData = RD;
+    numberOfHorses = raceData.getNumberOfHorses();
+    horses = new Image[numberOfHorses];
 
     this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
     this.setBackground(Color.green);
 
-    horse = new ImageIcon("C:\\Users\\hummu\\Documents\\Uni\\Year 1\\Semester B\\Object Oriented Programming\\Project\\HorseRaceSimulator\\Part2\\images\\placeholder.png").getImage();
+    // set placeholder images into image array
+    for (int i=0; i<numberOfHorses; i++) {
+      horses[i]= new ImageIcon("C:\\Users\\hummu\\Documents\\Uni\\Year 1\\Semester B\\Object Oriented Programming\\Project\\HorseRaceSimulator\\Part2\\images\\placeholder.png").getImage();
+    }
+
     timer = new Timer(100, this);
     timer.start();
   
   }
 
-  public void paint(Graphics g) {
-    super.paint(g); // paints bg
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g); // paints bg
     Graphics2D g2D = (Graphics2D) g;
-    g2D.drawImage(horse, x, y, null);
-    
 
+    // update each horse repeatedly
+    for (int  i=0; i<numberOfHorses; i++) {
+      g2D.drawImage(horses[i], xPos[i], yPos[i], null);
+    }
+    
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    if (x<= PANEL_WIDTH-80) {
-      x = x + xVelocity;
-      repaint();
+
+    // update each horses x position repeatedly
+    for (int i=0; i<numberOfHorses; i++) {
+      if (xPos[i]<= FINISH_LINE_X) {
+        xPos[i] = xPos[i] + xVelocity[i];
+      }
     }
+    repaint();
   }
 
 }
