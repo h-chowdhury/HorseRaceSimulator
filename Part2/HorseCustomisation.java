a3b123ffda98ca56b7534f3f92e74925c10c39ac
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-23 23:00:54 +0600

    Created HorseCustomisation class which stores attributes selected on HorseSelection window

---
+++ Part2/HorseCustomisation.java
@@ -0,0 +1,19 @@
+package Part2;
+
+/**
+ * Defines the customisable attributes of a Horse
+ * 
+ * @author Humayra Chowdhury
+ * @version Version 1.0
+ */
+public class HorseCustomisation {
+
+  private Horse horse;
+
+  private String breedType;
+  private String hairType;
+  private String saddleType;
+  private String horseshoeType;
+  private String accessoryType;
+
+}
