1b1d80442d8b075af35ec9a72c24a57c4ca5b2ec
Humayra Chowdhury <hc1415926@gmail.com>
2025-04-24 17:27:46 +0600

    Added main class which acts as the main entry point

---
+++ Part2/Main.java
@@ -0,0 +1,21 @@
+package Part2;
+
+/**
+ * This is the main class of the program.
+ * It currently acts as the main entry point.
+ * 
+ * @author Humayra Chowdhury
+ * @version Version 1.0
+ */
+public class Main
+{
+  /**
+   * Main method containing the creation of the LaunchPage.
+   * @param args An array of command-line arguments passed to the program.
+   */
+  public static void main(String[] args) 
+  {
+    new LaunchPage();
+  }
+
+}
