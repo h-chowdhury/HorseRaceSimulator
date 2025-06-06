260cb6250b8a38d70a5b780296dd8cbd8ce3aa35
Humayra Chowdhury <hc1415926@gmail.com>
2025-06-06 20:12:54 +0100

    Changes to GUI

--- Part2/LaunchPage.java
+++ Part2/LaunchPage.java
@@ -23,79 +23,58 @@ public class LaunchPage extends JFrame {
   
   public LaunchPage () {
 
-    // Title label
-    JLabel title = new JLabel("Welcome to the Horse Racing Simulator!");
-    title.setHorizontalAlignment(JLabel.CENTER);
-    title.setVerticalAlignment(JLabel.CENTER);
-    title.setFont(new Font("Dialog", Font.BOLD, 50));
-
-    // Description label
-    JLabel description = new JLabel("<html><body>This simulation runs <br>a race on a set of horses.<br> As the user, you will be<br>able to customise the track<br>and design your own horses<br>to participate in the race.</body></html>");
-    description.setHorizontalAlignment(JLabel.CENTER);
-    description.setVerticalAlignment(JLabel.CENTER);
-    description.setFont(new Font("Dialog", Font.BOLD, 25));
-
-    // Start button
-    JButton startButton = new JButton("Start Simulation");
-    startButton.setSize(200, 100);
-    startButton.setFont(new Font("Dialog", Font.BOLD, 30));
-    
     // Main frame
     JFrame frame = new JFrame();
     frame.setTitle("Eccentric Equestrian 2D"); 
     frame.setVisible(true); 
     frame.setSize(1500, 800);
     frame.setResizable(false); // prevents resizing window
-    frame.setLayout(new java.awt.GridLayout(2, 1, 5, 5));
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setBackground(Color.black);
 
     // Set window icon
     frame.setIconImage(new ImageIcon("Part2\\images\\windowIcon.png").getImage());
     
-    // Frame panels
-    JPanel titlePanel = new JPanel();
-    JPanel contentPanel = new JPanel();
 
-    titlePanel.setBackground(Color.white);
-    titlePanel.setPreferredSize(new Dimension(100, 100));
-    contentPanel.setPreferredSize(new Dimension(100, 100));
+      // Title label
+      JPanel titlePanel = new JPanel();
+        JLabel title = new JLabel("Welcome to Eccentric Equestrian 2D!");
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+        title.setFont(new Font("Dialog", Font.BOLD, 50));
+      titlePanel.add(title);
 
-    // Start button function - redirects to track selection page
-    startButton.addActionListener (new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        TrackSelection trackSelectionPage = new TrackSelection();
-        trackSelectionPage.setVisible(true);
-        frame.dispose();
-      }
-    });
 
-    // --------------------- sub panels ---------------------
+      JPanel contentPanel = new JPanel();
+      contentPanel.setLayout(new GridLayout(2, 1));
 
-    JPanel panel1 = new JPanel();
-    JPanel panel2 = new JPanel(); 
+        // Description label
+        JLabel description = new JLabel("<html><body>This simulation runs <br>a race on a set of horses.<br> As the user, you will be<br>able to customise the track<br>and design your own horses<br>to participate in the race.</body></html>");
+        description.setHorizontalAlignment(JLabel.CENTER);
+        description.setVerticalAlignment(JLabel.CENTER);
+        description.setFont(new Font("Dialog", Font.BOLD, 25));
 
-    panel1.setBackground(Color.gray);
-    panel2.setBackground(Color.darkGray);
+        // Start button
+        JButton startButton = new JButton("Start Simulation");
+        startButton.setSize(200, 100);
+        startButton.setFont(new Font("Dialog", Font.BOLD, 30));
 
-    contentPanel.setLayout(new GridLayout(1, 2, 5, 5));
+        // Start button function - redirects to track selection page
+        startButton.addActionListener (new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            TrackSelection trackSelectionPage = new TrackSelection();
+            trackSelectionPage.setVisible(true);
+            frame.dispose();
+          }
+        });
 
-    panel1.setPreferredSize(new Dimension(50, 50));
-    panel2.setPreferredSize(new Dimension(50, 50));
-
-    contentPanel.add(panel1);
-    contentPanel.add(panel2);
-
-    // -------------------------------------------------------
+      contentPanel.add(description);
+      contentPanel.add(startButton);
 
     frame.add(titlePanel);
     frame.add(contentPanel);
 
-    titlePanel.add(title);
-
-    panel1.add(description);
-    panel2.add(startButton);
   }
 
 }
